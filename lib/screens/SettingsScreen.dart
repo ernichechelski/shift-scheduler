4d820e98b829d6b09cd0bd06c123007e1d026743
Mariusz Brzeski <mariusz.brzeski@netguru.com>
2022-09-16 16:13:31 +0200

    Create Settings and Shifts screens

---
+++ lib/screens/SettingsScreen.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+class SettingsScreen extends StatelessWidget {
+  const SettingsScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.blueAccent,
+          title: Text('Settings'),
+          centerTitle: true,
+        ),
+        body: Center(
+            child: ElevatedButton(
+                onPressed: () => Navigator.pushNamedAndRemoveUntil(context, '/login', (route) => false),
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.blueAccent,
+                ),
+                child: const Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 70, vertical: 15),
+                  child: Text(
+                    'Logout',
+                    style: TextStyle(
+                      fontSize: 26,
+                      color: Colors.white,
+                    ),
+                  ),
+                ))));
+  }
+}
